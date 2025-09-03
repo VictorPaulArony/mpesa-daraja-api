3b45d0726ca35a2cc01463d9678ce25d7192541d
victorpaularony <smallypauls@gmail.com>
2025-09-03 20:56:42 +0300

    feat: an application using java to implement mpesa daraja api for stkpush using sandbox

---
+++ mpesaapi/src/main/java/com/example/mpesaapi/MpesaapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mpesaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpesaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpesaapiApplication.class, args);
+	}
+
+}
---
+++ mpesaapi/src/test/java/com/example/mpesaapi/MpesaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mpesaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
