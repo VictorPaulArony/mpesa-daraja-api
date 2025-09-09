1b03e18026e02be941d06f893ca336052ebc1d92
victorpaularony <smallypauls@gmail.com>
2025-09-09 06:47:47 +0300

    refactor: enhance B2BRequest with environment configuration and error handling

--- mpesaapi/src/main/java/com/example/mpesaapi/service/B2BRequest.java
+++ mpesaapi/src/main/java/com/example/mpesaapi/service/B2BRequest.java
@@ -6,20 +6,20 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
+import com.example.mpesaapi.config.MpesaConfigProperties;
 import com.example.mpesaapi.dto.B2BRequestDto;
 
 import lombok.Data;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import okhttp3.*;
 
 @Data
 @Service
 public class B2BRequest {
 
     private final MpesaService mpesaService; // Service to handle M-Pesa authentication
+    private static final String SANDURL = "https://sandbox.safaricom.co.ke";
+    private static final String PRODURL = "https://api.safaricom.co.ke";
+    private final MpesaConfigProperties config;
 
     public String sendB2BRequest(B2BRequestDto dto) throws IOException {
         JSONArray jsonArray = new JSONArray();
@@ -40,14 +40,14 @@ public class B2BRequest {
         jsonArray.put(jsonObject);
 
         String requestJson = jsonArray.toString().replaceAll("[\\[\\]]", "");
-        System.out.println(requestJson);
 
+        String baseUrl = isSandbox() ? SANDURL : PRODURL;
         OkHttpClient client = new OkHttpClient();
 
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType, requestJson);
         Request request = new Request.Builder()
-                .url("https://sandbox.safaricom.co.ke/safaricom/b2b/v1/paymentrequest")
+                .url(baseUrl + "/mpesa/stkpushquery/v1/query")
                 .post(body)
                 .addHeader("content-type", "application/json")
                 .addHeader("authorization", "Bearer " + mpesaService.authenticate())
@@ -56,6 +56,14 @@ public class B2BRequest {
                 .build();
 
         Response response = client.newCall(request).execute();
-        return response.body().string();
+        ResponseBody finalBody = response.body(); // Read response body
+            if (finalBody == null) {
+                throw new IOException("Response body is null");
+            }
+            return finalBody.string();
+    }
+
+    private boolean isSandbox() {
+        return "sandbox".equalsIgnoreCase(config.getEnv());
     }
 }
