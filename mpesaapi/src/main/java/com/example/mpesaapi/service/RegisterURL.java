263a1417af3790c0e0e025ff845c77b6953c767f
victorpaularony <smallypauls@gmail.com>
2025-09-09 06:49:14 +0300

    refactor: add environment config and improve error handling in RegisterURL

--- mpesaapi/src/main/java/com/example/mpesaapi/service/RegisterURL.java
+++ mpesaapi/src/main/java/com/example/mpesaapi/service/RegisterURL.java
@@ -4,16 +4,22 @@ import java.io.IOException;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.stereotype.Service;
 
+import com.example.mpesaapi.config.MpesaConfigProperties;
 import com.example.mpesaapi.dto.RegisterURLDto;
 
 import lombok.RequiredArgsConstructor;
 import okhttp3.*;
 
 @RequiredArgsConstructor
+@Service
 public class RegisterURL {
 
     private final MpesaService mpesaService; // Service to handle M-Pesa authentication
+    private static final String SANDURL = "https://sandbox.safaricom.co.ke";
+    private static final String PRODURL = "https://api.safaricom.co.ke";
+    private final MpesaConfigProperties config;
 
     public String registerURL(RegisterURLDto dto) throws IOException {
         JSONArray jsonArray = new JSONArray();
@@ -26,14 +32,13 @@ public class RegisterURL {
         jsonArray.put(jsonObject);
 
         String requestJson = jsonArray.toString().replaceAll("[\\[\\]]", "");
-        System.out.println(requestJson);
-
+        String baseUrl = isSandbox() ? SANDURL : PRODURL;
         OkHttpClient client = new OkHttpClient();
 
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType, requestJson);
         Request request = new Request.Builder()
-                .url("https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl")
+                .url(baseUrl + "/mpesa/c2b/v1/registerurl")
                 .post(body)
                 .addHeader("content-type", "application/json")
                 .addHeader("authorization", "Bearer " + mpesaService.authenticate())
@@ -41,7 +46,15 @@ public class RegisterURL {
                 .build();
 
         Response response = client.newCall(request).execute();
-        System.out.println(response.body().string());
-        return response.body().string();
+       
+        ResponseBody responseBody = response.body();
+        if (responseBody == null) {
+            throw new IOException("Response body is null");
+        }
+        return responseBody.string();
+    }
+
+    private boolean isSandbox() {
+        return "sandbox".equalsIgnoreCase(config.getEnv());
     }
 }
